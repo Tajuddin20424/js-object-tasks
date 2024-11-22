d453b78ef6425b522111bfdbc2b566ced3db6037
Tajuddin20424 <mttajuddin01@gmail.com>
2024-11-22 23:53:02 +0600

    added the all js

---
+++ keyValue.js
@@ -0,0 +1,52 @@
+// ### Task-5 (Hard)
+
+// Loop through an object and print the key-value pairs with their `types`
+
+// **Input:**
+// <br>
+
+// let myObject = {
+
+//     name: 'John Doe',
+//     age: 25,
+//     city: 'Example City',
+//     isStudent: true
+
+// };
+
+// **Output:**
+
+// <br>
+
+//     key: name | type:  string
+//     key: age | type:  number
+//     key: city | type:  string
+//     key: isStudent | type:  boolean
+
+
+
+// let myObject = {
+//     name: 'John Doe',
+//     age: 25,
+//     city: 'Example City',
+//     isStudent: true
+// };
+
+// for (let key in myObject) {
+//     console.log(`key: ${key} | type: ${typeof myObject[key]}`);
+// }
+
+
+
+
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Dhaka',
+    isStudent: true
+}
+
+for (let key in myObject) {
+    console.log(`key: ${key} | type: ${typeof myObject[key]}`);
+}
+
